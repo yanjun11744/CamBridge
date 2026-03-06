de74da59becd1448832ed78341a8b52e91b5161c
yanjun11744 <sunyanjun11744@163.com>
2026-03-06 21:30:14 +0800

    Initial Commit

---
+++ CamBridge/CamBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamBridgeApp.swift
+//  CamBridge
+//
+//  Created by Yanjun Sun on 2026/3/6.
+//
+
+import SwiftUI
+
+@main
+struct CamBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamBridge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CamBridge
+//
+//  Created by Yanjun Sun on 2026/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
